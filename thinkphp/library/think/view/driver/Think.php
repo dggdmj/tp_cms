fe9159f13b6599645e5db6642ce26827fcf83cfb
dggdmj@163.com <dggdmj@163.com>
2017-06-23 11:45:22 +0800

    增加了手机端和PC端的识别

--- thinkphp/library/think/view/driver/Think.php
+++ thinkphp/library/think/view/driver/Think.php
@@ -39,12 +39,103 @@ class Think
     public function __construct($config = [])
     {
         $this->config = array_merge($this->config, $config);
-        if (empty($this->config['view_path'])) {
-            $this->config['view_path'] = App::$modulePath . 'view' . DS;
+        // if (empty($this->config['view_path'])) {
+        //     $this->config['view_path'] = App::$modulePath . 'view' . DS;
+        // }
+     
+     //增加手机版访问
+        if($this->is_mobile()===true){      //手机版
+            if(isset($this->config['m_view_path'])){
+                $this->config['view_path'] = $this->config['m_view_path'];
+            }else{
+                $this->config['view_path'] = App::$modulePath . 'mobile' . DS;
+            }
+        }else{  //PC访问
+            if (empty($this->config['view_path'])) {
+                $this->config['view_path'] = App::$modulePath . 'view' . DS; 
+            }else{
+                $this->config['view_path'] = config('template.view_path');
+            }
         }
-
         $this->template = new Template($this->config);
     }
+
+
+//php判断手机移动设备访问，返回true是移动设备
+    private function is_mobile()
+    {
+// 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+        if (isset ($_SERVER['HTTP_X_WAP_PROFILE']))
+        {
+            return true;
+        }
+// 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+        if (isset ($_SERVER['HTTP_VIA']))
+        {
+// 找不到为flase,否则为true
+            return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+        }
+// 脑残法，判断手机发送的客户端标志,兼容性有待提高
+        if (isset ($_SERVER['HTTP_USER_AGENT']))
+        {
+            $clientkeywords = array (
+                'nokia',
+                'oppo',
+                'xiaomi',
+                'miui',
+                'huawei',
+                'coolpad',
+                'sony',
+                'ericsson',
+                'mot',
+                'samsung',
+                'htc',
+                'sgh',
+                'lg',
+                'sharp',
+                'sie-',
+                'philips',
+                'panasonic',
+                'alcatel',
+                'lenovo',
+                'iphone',
+                'ipod',
+                'blackberry',
+                'meizu',
+                'android',
+                'netfront',
+                'symbian',
+                'ucweb',
+                'windowsce',
+                'palm',
+                'operamini',
+                'operamobi',
+                'openwave',
+                'nexusone',
+                'cldc',
+                'midp',
+                'wap',
+                'mobile'
+            );
+// 从HTTP_USER_AGENT中查找手机浏览器的关键字
+            if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT'])))
+            {
+                return true;
+            }
+        }
+// 协议法，因为有可能不准确，放到最后判断
+        if (isset ($_SERVER['HTTP_ACCEPT']))
+        {
+// 如果只支持wml并且不支持html那一定是移动设备
+// 如果支持wml和html但是wml在html之前则是移动设备
+            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html'))))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     /**
      * 检测是否存在模板文件
